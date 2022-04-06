4c680c0c396cd3b44ee8cb7904dd0c62cd7a1aa8
Raul Sampedro <rsrdesarrollo@gmail.com>
2022-04-06 11:12:57 +0200

    pump version

--- version/info.go
+++ version/info.go
@@ -1,6 +1,6 @@
 package version
 
-var VersionTag = "v1.0.2"
+var VersionTag = "v1.0.3"
 var ToolName = "SaSSHimi"
 var Author = "@rsrdesarrollo"
 var RepoURL = "https://github.com/rsrdesarrollo/SaSSHimi"
